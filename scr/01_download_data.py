21e7b15b3dc897ea0b31abe70ec556aba20b08fe
danielvo-finance <danielvo.bus@gmail.com>
2026-01-11 14:35:56 -0600

    Quick outline of download data script

---
+++ scr/01_download_data.py
@@ -0,0 +1,12 @@
+"""
+Note:
+- The sole purpose of this script is for the user to input the asset ticker(s) that they desire to analyze.
+- Once the ticker is inputted, the data will be downloaded as a csv in data/raw/.
+- The data should NOT be modified once downloaded.
+"""
+
+import yfinance as yf
+import os
+
+def download_data():
+    pass
