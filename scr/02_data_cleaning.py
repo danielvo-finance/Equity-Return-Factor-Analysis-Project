6b090bb3f57e0dcb4617bed3802adc6adb54de3d
danielvo-finance <danielvo.bus@gmail.com>
2026-01-12 22:05:29 -0600

    Update 02_data_cleaning.py

--- scr/02_data_cleaning.py
+++ scr/02_data_cleaning.py
@@ -10,24 +10,26 @@ import os
 
 RAW_DIR = "data/raw"
 PROCESSED_DIR = "data/processed"
-
 file = [f for f in os.listdir(RAW_DIR) if f.endswith(".csv")]
-for f in file:
-    ticker = f.split(".")[0]
 
-def compute_returns(ticker):
+def compute_returns(file):
     os.makedirs(PROCESSED_DIR, exist_ok=True)
-    
+    ticker = file.replace(".csv", "")
+
     df = pd.read_csv(
-        os.path.join(RAW_DIR, ticker),
+        os.path.join(RAW_DIR, file),
+        skiprows=3,
         header=None,
         names = ["Date", "Close", "High", "Low", "Open", "Volume"],
         index_col=0,
         parse_dates=True)
     
-    df["Close"] = df[["Close"]].copy()
+    df = df[["Close"]].copy()
     df["Close"] = pd.to_numeric(df["Close"], errors="coerce")
+
     df["Return"] = df["Close"].pct_change()
+    df = df.dropna()
+
 
     df.to_csv(f"{PROCESSED_DIR}/{ticker}_processed.csv")
     print(f"Saved processed data for {ticker}")
